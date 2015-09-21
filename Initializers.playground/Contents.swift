e1b4b9d63577a8cd31df1f16eeaf64b8f44603e7
Howlinmoon <github@strifecrafter.com>
2015-09-21 19:09:53 -0400

    Initializers, custom and standard, along with a deinit

--- Initializers.playground/Contents.swift
+++ Initializers.playground/Contents.swift
@@ -4,11 +4,41 @@ import UIKit
 
 class Player {
     //properties
-    var name: String = "John Doe"
-    var score: Int = 0
+    var name: String
+    var score: Int
     
     // Methods
     func description() -> String {
         return ("Player \(name) has a score of \(score)")
     }
+    
+    // default initializer (constructor)
+    // no func needed
+    init() {
+        name = "John Doe"
+        score = 0
+    }
+    // an intializer with a parameter - a custom init
+    init(name: String) {
+        // set the local instance name to the parameter name
+        self.name = name
+        self.score = 0
+    }
+    
+    // one deinitializer per class
+    // called when destroying an instance
+    deinit {
+        // clean up code goes in here
+        // called automatically when needed - NOT by the user
+    }
 }
+
+// instantiate a user
+var firstPlayer = Player() // init is called automatically
+print(firstPlayer.description())
+
+
+// use the custom initializer
+var secondPlayer = Player(name: "Alice")
+print(secondPlayer.description())
+
