a55416107458c0f840ac95c1bc6965d58e757923
Jim Veneskey <github@strifecrafter.com>
2015-09-23 12:16:08 -0400

    Swift Operators

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,31 @@
+//: Playground - operators
+import UIKit
+
+var a = 5
+var b = 50
+var c = 11.38
+var highScore = 500000
+
+var result = a + b
+result = b - 20
+var x = c * 3.33
+x = c / 1.1
+
+highScore = highScore + 5
+
+highScore += 50
+
+// experimenting with POSTfix and PREfix
+
+var score = 99
+
+score++
+score
+score--
+score
+
+// Prefix version
+
+--score
+++score
+
